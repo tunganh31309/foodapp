5181a8fb8a879f78b222945e7e8212a35454db89
TUNG ANH <chuthanhtunganh98@gmail.com>
2020-12-23 13:54:03 +0700

    thêm code vào file

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:foodapp/models/category.dart';
 
+// ignore: must_be_immutable
+class CategoryItem extends StatelessWidget {
+  //1 categoryItem - 1 category object
+  //  1 cái categoryItem tương ứng với 1 một object
+  Category category;
+  CategoryItem({this.category}); // Định nghĩa thuộc tính Category
+  @override
+  Widget build(BuildContext context) {
+    Color _color = this.category.color;
+    return Container(
+        child: Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(this.category.content,
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)),
+        ],
+      ),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [_color.withOpacity(0.8), _color],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft),
+          color: _color,
+          borderRadius: BorderRadius.circular(20)),
+    ));
+  }
+}
