5fc354aac407e5e61ed72987f86b653d5fffbb43
TUNG ANH <chuthanhtunganh98@gmail.com>
2020-12-23 09:50:27 +0700

    thêm commit

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -7,5 +7,6 @@ class Category {
   final Color color; // màu sắc
   // final nghĩa là giá trị chỉ xét 1 lần không tham chiếu sang thằng khác
   const Category({@required this.id, @required this.content, this.color});
+  // id và content cho required bởi bắt buộc phải nhập vào
 }
 // thực thể các đối tượng ở đây chẳng hạn như là danh sách các món ăn, danh sách các thể loại món ăn
