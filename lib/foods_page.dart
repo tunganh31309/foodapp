0752691fb7e14966069d2555c66c0c8871d3d073
TUNG ANH <chuthanhtunganh98@gmail.com>
2020-12-23 22:30:20 +0700

    Hiển thị chi tiết đối tượng Food,

--- lib/foods_page.dart
+++ lib/foods_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:foodapp/fake_data.dart';
 import 'package:foodapp/models/category.dart';
 import 'package:foodapp/models/food.dart';
+import 'detail_food_page.dart';
 
 class FoodsPage extends StatelessWidget {
   static const String routeName = '/FoodsPage';
@@ -21,87 +22,106 @@ class FoodsPage extends StatelessWidget {
       ),
       body: Center(
           child: Center(
-        child: ListView.builder(
-            itemCount: foods.length,
-            itemBuilder: (context, index) {
-              Food food = foods[index];
-              return Stack(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      clipBehavior: Clip.hardEdge,
-                      child: Center(
-                        child: FadeInImage.assetNetwork(
-                            placeholder:
-                                'assets/images/loading.gif', // ảnh khi mình loading
-                            image: food.urlImage), // đường link đến cái ảnh đó
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    // Đặt vị trí tuyện đối với Positioned của cái thẻ đó so với thẻ khác
-                    top: 30,
-                    left: 30,
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          color: Colors.black45,
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Colors.white, width: 2)),
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.timer,
-                            color: Colors.white,
-                            size: 25,
-                          ), // Icon hiện thời gian lấy từ fake_data
-                          Text(
-                            '${food.duration.inMinutes} minutes',
-                            style: TextStyle(fontSize: 22, color: Colors.white),
+        child: foods.length > 0
+            ? ListView.builder(
+                itemCount: foods.length,
+                itemBuilder: (context, index) {
+                  //We will continue in the next lesson
+                  Food food = foods[index];
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => DetailFoodPage(
+                              food: food) //send params using "constructor"
+                          ));
+                    },
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(20),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            clipBehavior: Clip.hardEdge,
+                            child: Center(
+                              child: FadeInImage.assetNetwork(
+                                  placeholder:
+                                      'assets/images/loading.gif', // ảnh khi đang loading
+                                  image: food
+                                      .urlImage), // đường link đến ảnh mình chọn
+                            ),
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 30,
-                    right: 30,
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Colors.greenAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            '${food.complexity.toString().split('.').last}', // độ khó của món ăn
-                            style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                        Positioned(
+                          //Đặt vị trí tuyện đối với Positioned của cái thẻ đó so với thẻ khác
+                          top: 30,
+                          left: 30,
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: Colors.black45,
+                                borderRadius: BorderRadius.circular(10),
+                                border:
+                                    Border.all(color: Colors.white, width: 2)),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.timer,
+                                  color: Colors.white,
+                                  size: 25,
+                                ), // Icon hiện thời gian lấy từ fake_data
+                                Text(
+                                  '${food.duration.inMinutes} minutes',
+                                  style: TextStyle(
+                                      fontSize: 22, color: Colors.white),
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                        Positioned(
+                          top: 30,
+                          right: 30,
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.greenAccent,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  '${food.complexity.toString().split('.').last}',
+                                  style: TextStyle(
+                                      fontSize: 22, color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 30,
+                          right: 30,
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: Colors.black45,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(
+                              '${food.name}',
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  Positioned(
-                    bottom: 30,
-                    right: 30,
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Colors.black45,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        '${food.name}', // tên món ăn
-                        style: TextStyle(fontSize: 30, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            }),
+                  );
+                })
+            : Text(
+                'No foods found',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
       )),
     );
   }
