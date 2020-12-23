76fe824097fe5bcaa02d69e4ed7bdf3728b93e76
TUNG ANH <chuthanhtunganh98@gmail.com>
2020-12-23 14:23:16 +0700

    thêm vài category nữa vào file fake_data

--- lib/fake_data.dart
+++ lib/fake_data.dart
@@ -5,7 +5,10 @@ const FAKE_CATEGORIES = const [
   Category(id: 1, content: 'Japanese\'s Foods', color: Colors.deepOrange),
   Category(id: 2, content: 'Pizza', color: Colors.teal),
   Category(id: 3, content: 'Humburgers', color: Colors.cyanAccent),
+  Category(id: 4, content: 'Italian', color: Colors.blueAccent),
+  Category(id: 5, content: 'Milk & Yoghurt', color: Colors.deepPurple),
+  Category(id: 6, content: 'Vegetables', color: Colors.green),
+  Category(id: 7, content: 'Fruits', color: Colors.redAccent),
   // id cho từ 1 trở đi rồi tăng lên 1
-  
 ];
 // Những dữ liệu mình tự bịa đặt
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.cyan),
       home: Scaffold(
           appBar: AppBar(
-//          centerTitle: false,
+            centerTitle: true,
             title: const Text('Food\'s categories'),
           ),
           body: SafeArea(
