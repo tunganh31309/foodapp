44c9ee1770734d8cb0e8a04a3486c7f70986c0ee
TUNG ANH <chuthanhtunganh98@gmail.com>
2020-12-23 15:51:46 +0700

    thêm điều kiện

--- lib/foods_page.dart
+++ lib/foods_page.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:foodapp/fake_data.dart';
 import 'package:foodapp/models/category.dart';
+import 'package:foodapp/models/food.dart';
 
 class FoodsPage extends StatelessWidget {
-  static const String routeName = '/FoodsPage'; // thêm thuộc tính
+  static const String routeName = '/FoodsPage';
   Category category;
   FoodsPage({this.category});
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     Map<String, Category> arguments = ModalRoute.of(context).settings.arguments;
-    this.category = arguments['category']; // tên của cái key
+    this.category = arguments['category'];
     //Filter foods of from category
+    List<Food> foods = FAKE_FOODS
+        .where((food) => food.categoryId == this.category.id)
+        .toList();// điều kiện thỏa mãn để lôi 
     return Scaffold(
       appBar: AppBar(
         title: Text('Foods from ${category.content}'),
       ),
-      body: Center(
-        child: Text(
-          'This show Foods\'s list',
-          style: TextStyle(fontSize: 25),
-        ),
-      ),
+      body: Center(child: Center(
+        child: ListView.builder(itemBuilder: (context, index) {
+          
+        }),
+      )),
     );
   }
 }
